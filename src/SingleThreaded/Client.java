d59d2a47f043bb21244c6032ec59fde2014993e8
Amitosh <amitosh.verma9@gmail.com>
2024-02-16 23:19:05 +0530

    done with multithread and compare it with single threaded with jmeter
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/SingleThreaded/Client.java
+++ src/SingleThreaded/Client.java
@@ -8,18 +8,17 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.net.Socket;
-import java.security.spec.ECField;
 
 public class Client {
     public void run() throws UnknownHostException, IOException {
         int port = 8010;
-        InetAddress address = InetAddress.getByName("localhost");
-        Socket socket = new Socket(address, port);
-        PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
-        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        toSocket.println("hello from the client");
-        String line = fromSocket.readLine();
-        System.out.println("Response from the socket is : "+line);
+            InetAddress address = InetAddress.getByName("localhost");
+            Socket socket = new Socket(address, port);
+            PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
+            BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            toSocket.println("hello from the client");
+            String line = fromSocket.readLine();
+            System.out.println("Response from the socket is : " + line);
         toSocket.close();
         fromSocket.close();
         socket.close();
